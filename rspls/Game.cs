93c6d9fa98bd74c372ca69eaf17f9c31660eadc1
Fowley <alexanderfowle@gmail.com>
2020-04-14 12:35:42 -0500

    Created classes; implied relationships; built gestures List<T>

---
+++ rspls/Game.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace rspls
+{
+    class Game
+    {
+        public List<string> Gestures;
+
+        public Game()
+        {
+            Gestures = new List<string>()
+            {
+                "Rock",
+                "Paper",
+                "Scissors",
+                "Lizard",
+                "Spock"
+            };
+        }
+    }
+}
---
+++ rspls/Player.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace rspls
+{
+    class Player
+    {
+        protected string name;
+
+        public Player()
+        {
+
+        }
+    }
+}
